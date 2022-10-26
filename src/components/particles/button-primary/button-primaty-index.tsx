906565f3d2be348bba13906a0d732f370b138dbd
MateusMontan <mateusinfocefetmg@gmail.com>
2022-10-26 14:20:22 -0300

    Update button-primaty-index.tsx
    Adding just a comment.

--- src/components/particles/button-primary/button-primaty-index.tsx
+++ src/components/particles/button-primary/button-primaty-index.tsx
@@ -7,6 +7,7 @@ type ListButtonProps = {
 }
 
 //LEMBRAR DE MODIFICIAR STYLE, CORES PASSADAS DIRETAMENTE PARA O STYLED BUTTON
+//BOTÃO PRINCIPAL AZUL - 'ENTRAR', 'AVANÇAR', 'ATIVAR CONTA', 'FAZER LOGIN'...
 export default function ButtonParticle(props : ListButtonProps) {
   return (
     <StyledButton>{props.text}</StyledButton>
