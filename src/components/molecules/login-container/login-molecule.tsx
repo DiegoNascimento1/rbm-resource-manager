7d010a1253fcec5c031c421a82c6bf0584a57c00
Rhuan Freire <rhuan.freire@rbmweb.com.br>
2022-10-26 20:12:29 -0300

    minor fixes

--- src/components/molecules/login-container/login-molecule.tsx
+++ src/components/molecules/login-container/login-molecule.tsx
@@ -1,9 +1,11 @@
 import InputContainer from "components/atoms/input-container";
+import LogoParticles from "components/particles/logo-particles/logo-index";
 import * as Styled from "./login-molecule-style";
 
 export default function LoginMolecule() {
   return (
     <Styled.LoginContainer>
+      <LogoParticles />
       <InputContainer labelName={"E-mail"} password={false}/>
       <InputContainer labelName={"Senha"} password={true}/>
     </Styled.LoginContainer>
