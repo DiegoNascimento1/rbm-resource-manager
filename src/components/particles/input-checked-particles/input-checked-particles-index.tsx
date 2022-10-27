e01d259917ebe5858c3a707a7106b0a9e7cd5958
marcello-junqueira <marcellojunqueira@hotmail.com>
2022-10-27 17:07:26 -0300

    criando o input checked e label login

---
+++ src/components/particles/input-checked-particles/input-checked-particles-index.tsx
@@ -0,0 +1,10 @@
+import { StyledInput } from "./input-checked-particles-style";
+
+export default function InputCheckedParticles() {
+
+  return (
+    <>
+      <StyledInput type={"checkbox"}  />
+    </>
+  );
+}
