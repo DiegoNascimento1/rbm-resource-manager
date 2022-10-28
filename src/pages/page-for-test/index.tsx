dfacb77076785f9f5a414db30c1c02c4e1e2c624
Mateus Montan <mateusinfocefetmg@gmail.com>
2022-10-28 02:51:51 -0300

    feat/recovery-sucess-container
    Frame 8 - Sucess password change ( recovery )

--- src/pages/page-for-test/index.tsx
+++ src/pages/page-for-test/index.tsx
@@ -4,12 +4,12 @@ import React from "react";
 import AlertAtom from "components/atoms/alert-notification-atom/alert-notification-index";
 import ListParticleColumn from "components/particles/ul-list-column-particle/list-index";
 import ListParticleRow from "components/particles/ul-list-row-particle/list-index";
+import RecoverySucessContainer from "components/molecules/recovery-password-sucess/recovery-sucess";
 
 export default function MateusTestPage() {
   return (
     <>
-      <ListParticleColumn/>
-      <ListParticleRow/>
+      <RecoverySucessContainer/>
     </>
   )
 
