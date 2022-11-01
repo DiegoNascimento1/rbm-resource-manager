71232c1a9c653eab7a18fdcc65323cd7f37f78d3
Mateus Montan <mateusinfocefetmg@gmail.com>
2022-11-01 19:17:55 -0300

    fix/Change how the Button Primary On Click function works.

--- src/components/molecules/login-container/login-container.tsx
+++ src/components/molecules/login-container/login-container.tsx
@@ -7,13 +7,36 @@ import LogoSvg from "components/particles/logo-svg/logo-svg";
 import Spacing from "../../particles/spacing-particles/index";
 import LoginChecked from "components/atoms/login-checked-link";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 export default function LoginContainer() {
 
   const[login,setLogin] = useState("");
   const[senha,setSenha] = useState("");
 
+  const navigate = useNavigate();
 
+  async function Logando(loginInput: string, senhaInput: string) {
+    
+    if (loginInput && senhaInput) {
+      if (loginInput.length > 0 && senhaInput.length > 0) {
+        try {
+          let requisicao = await axios.post('https://agendamento.carloschoma.com.br/login', {
+            'login': loginInput,
+            'senha': senhaInput
+          });
+          console.log(requisicao.data);
+          navigate('/mateus-test');
+        } catch {
+          console.log('login/senha incorreta');
+        }
+      }
+    }
+    else {
+      //CASO NÂO TENHA NADA DIGITADO NO CAMPO EMAIL OU SENHA
+    }
+  }
 
   return (
     <Styled.LoginContainer>
@@ -26,7 +49,7 @@ export default function LoginContainer() {
       <InputContainer setInput={setLogin} placeholder={"Digite seu e-mail"} labelName={"E-mail"} password={false}/>
       <InputContainer setInput={setSenha} placeholder={"Digite sua senha"} labelName={"Senha"} password={true}/>
       <LoginChecked textoLabel={"Salvar login"} textoLink={"Esqueci a senha"} textoHref={"https://www.google.com.br/"}/>
-      <ButtonParticle text={"ENTRAR"} loginInput={login} senhaInput={senha}/>
+      <ButtonParticle text={"ENTRAR"} onClick={()=>Logando(login, senha)}/>
       <Spacing marginTop={"32px"}/>
       <Typography tag={'p'} size={'14px'} margin={"0px"} decoration={'underline'} fontWeight={"400"}>Primeiro acesso? <a href="#" style={{fontWeight:"600",color:"black"}}>Ative sua conta.</a></Typography>
       <Spacing marginTop={"32px"}/>
