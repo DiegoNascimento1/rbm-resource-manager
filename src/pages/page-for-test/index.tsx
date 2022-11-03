c05b1af181a55675b891ed2f88398db4c65c84dc
MateusMontan <mateusinfocefetmg@gmail.com>
2022-11-03 16:44:33 -0300

    fix/how input field works
    bonus/add button logout

--- src/pages/page-for-test/index.tsx
+++ src/pages/page-for-test/index.tsx
@@ -1,10 +1,15 @@
 import ScheduleContainer from "components/molecules/schedule-container/schedule-container";
+import ButtonParticle from "components/particles/button-primary/button-primaty-index";
+import { useAuth } from "contexts/auth-context";
 
 export default function MateusTestPage() {
+
+  const { logout } = useAuth();
 
   return (
     <>
       <ScheduleContainer/>
+      <ButtonParticle light onClick={()=>logout()} tamanho={'300px'} text='LOGOUT'/>
     </>
   )
 
