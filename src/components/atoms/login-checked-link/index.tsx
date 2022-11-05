24df1fc2cd8d8a7dce03df1ae12eeae63d133efd
Marcello-Junqueira <marcellojunqueira@hotmail.com>
2022-11-04 21:22:49 -0300

    Alteração nos componentes de Login Checked
    Tiramos a Tag A e passamos para a função Typography-particles, e acrescentamos no input-checked o clique  gerando o cheked

--- src/components/atoms/login-checked-link/index.tsx
+++ src/components/atoms/login-checked-link/index.tsx
@@ -1,15 +1,22 @@
 import { StyledDivContainer, StyledDivSubContainer } from "./style";
 import Typography from "components/particles/typography-particles";
 import InputChecked from "components/particles/input-checked";
-import LinkA from "components/particles/link-a";
+// import LinkA from "components/particles/link-a";
+import {THEME} from "contants";
 
 type propsLoginChecked = {
     textoLabel:string;
     textoLink:string;
-    textoHref:string;
+    setRecoverPassaword: React.Dispatch<React.SetStateAction<boolean>>;
+    RecoverPassaword:boolean;
+    // textoHref:string;
 };
 
 export default function LoginChecked(props:propsLoginChecked) {
+  function fxReviewPassaword(){
+    alert("esqueci a senha")
+    props.setRecoverPassaword(!props.RecoverPassaword);
+  }
   return (
     <StyledDivContainer>
 
@@ -17,9 +24,22 @@ export default function LoginChecked(props:propsLoginChecked) {
           <InputChecked textoLabel={props.textoLabel}/>          
         </StyledDivSubContainer>
 
-        <StyledDivSubContainer>
-          <Typography tag={'p'} size={'14px'} margin={"0px"} decoration={'underline'} fontWeight={"400"}>
-              <LinkA texto={props.textoLink} href={props.textoHref}></LinkA>
+        <StyledDivSubContainer onClick={()=> fxReviewPassaword()}>
+          <Typography 
+              tag={'p'} 
+              size={'14px'} 
+              margin={"0px"} 
+              decoration={'underline'} 
+              fontWeight={"500"}
+              color={THEME.light.colors.secondary3}
+              cursorHover={'pointer'}
+          >
+              {props.textoLink} 
+              {/* <LinkA 
+                texto={props.textoLink} 
+                href={props.textoHref} 
+                color={THEME.light.colors.secondary3}
+              ></LinkA> */}
           </Typography>
         </StyledDivSubContainer>
         
