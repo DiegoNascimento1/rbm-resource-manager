261a49d46b74b0bdc75c1f2e12d083667cd3ccfd
Marcello-Junqueira <marcellojunqueira@hotmail.com>
2022-11-22 03:59:24 -0300

    fix - troca de props por context
    criamos um novo conponente
    criamos um provider, e colocamos no main container
    tiramos as props do componente login container e passamos a passa por context
    falta acabar de colocar as conecçòes

--- src/components/atoms/password-master/index.tsx
+++ src/components/atoms/password-master/index.tsx
@@ -4,30 +4,26 @@ import Typography from "components/particles/typography-particles";
 import LogoSvg from "components/particles/logo-svg/logo-svg";
 import Spacing from "components/particles/spacing-particles";
 import LoginChecked from "components/atoms/login-checked-link";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useAuth } from "contexts/auth-context";
 import { fxRegexValidateUppercase, fxRegexValidateSpecialCharacters, fxRegexValidateNumber, fxRegexValidateEmail} from "functions/regex-validate";
 import { contextLogin } from "contexts/login-context";
 
-type PropsPasswordMaster= {
-  recoverPassword: boolean;
-  setRecoverPassword: React.Dispatch<React.SetStateAction<boolean>>;
-  setActivateAccount: React.Dispatch<React.SetStateAction<boolean>>;
-}
 
-export default function PasswordMaster(props:PropsPasswordMaster) {
+export default function PasswordMaster() {
 
   const[loginInput,setLogin] = useState("");
   const[senhaInput,setSenha] = useState("");
   const{login}  = useAuth();
 
-  const loginContext = useContext(contextLogin);
+  const contextoLogin = useContext(contextLogin);
+
+  const fxLogin = contextoLogin.funcoes?.setState;
 
   function handleLogin() {
-    //  fazer uma verificação
     const validatedEmail = loginInput.length >=6;
-    // const validatedEmail = fxRegexValidateEmail(loginInput);
     const validateSenha = senhaInput.length >=8 && fxRegexValidateNumber(senhaInput) && fxRegexValidateUppercase(senhaInput) && fxRegexValidateSpecialCharacters(senhaInput);
+    // const validatedEmail = fxRegexValidateEmail(loginInput);
 
     !validatedEmail && alert("Email errado");
     !validateSenha && alert("Senha fora do padrão")
@@ -36,9 +32,9 @@ export default function PasswordMaster(props:PropsPasswordMaster) {
   }
 
   function ativar(){
-    props.setActivateAccount(true)
-    props.setRecoverPassword(true)
-  }
+    fxLogin?.setChangeRecoverPassword(true);
+    fxLogin?.setChangeActiveAccount(true);
+  };
 
   useEffect(()=>{
     // loginContext.dadosData?.fx.fxTrocarNome('Trocado por objeto contruido');
@@ -48,14 +44,13 @@ export default function PasswordMaster(props:PropsPasswordMaster) {
     <>
       <LogoSvg/>
       <Spacing marginTop={"54px"}/>
-      {/* <h1>{loginContext.dadosData?.dados.usuario}</h1> */}
       <Typography tag={'p'} size={'20px'} margin={"0px"} fontWeight={"700"}>Entrar na minha conta</Typography>
       <Spacing marginTop={"10px"}/>
       <Typography tag={'p'} size={'14px'} margin={"0px"} fontWeight={"400"}>Acesse sua conta abaixo =)</Typography>
       <Spacing marginTop={"32px"}/>
       <InputContainer setInput={setLogin} placeholder={"email@rbmweb.com.br"} labelName={"E-mail"} password={false} elementFocus={true}/>
       <InputContainer setInput={setSenha} placeholder={"Digite sua senha"} labelName={"Senha"} password={true}/>
-      <LoginChecked textoLabel={"Salvar login"} textoLink={"Esqueci a senha"} RecoverPassaword={props.recoverPassword} setRecoverPassword={props.setRecoverPassword} />
+      <LoginChecked textoLabel={"Salvar login"} textoLink={"Esqueci a senha"} />
       <ButtonParticle light text={'ENTRAR'} onClick={()=>handleLogin()}/>
       <Spacing marginTop={"32px"}/>
       <Typography tag={'p'} size={'14px'} margin={"0px"} decoration={'underline'} fontWeight={"400"}>
--- src/components/molecules/main-container/main-container.tsx
+++ src/components/molecules/main-container/main-container.tsx
@@ -2,13 +2,17 @@ import LoginContainer from "components/molecules/login-container";
 import { useTheme } from "styled-components";
 import * as Styled from "./main-container-style";
 import InformationContainer from "components/molecules/information-container-molecules";
+import LoginProvider, { contextLogin } from "contexts/login-context";
+import { useContext } from "react";
 
 export default function MainContainer() {
 	const theme = useTheme();
 	return (
 		<Styled.MainContainer>
 			<InformationContainer />
-			<LoginContainer />
+			<LoginProvider>
+				<LoginContainer />
+			</LoginProvider>
 		</Styled.MainContainer>
 	);
 }
